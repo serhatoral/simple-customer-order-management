af2f667816fed1a62b27c5c238c946f388180d01
Serhat <developer.serhat@hotmail.com>
2024-01-12 21:43:26 +0300

    create customer controller and add getCustomerDtoById

--- src/main/java/dev/serhat/customerOrder/service/CustomerService.java
+++ src/main/java/dev/serhat/customerOrder/service/CustomerService.java
@@ -1,10 +1,14 @@
 package dev.serhat.customerOrder.service;
 
 
+import dev.serhat.customerOrder.dto.CustomerDto;
+import dev.serhat.customerOrder.dto.converter.CustomerDtoConverter;
 import dev.serhat.customerOrder.exception.CustomerNotFoundException;
 import dev.serhat.customerOrder.model.Customer;
 import dev.serhat.customerOrder.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -15,11 +19,17 @@ public class CustomerService {
         DI gerçekleştiğinden servisin interface'ini oluşturmadım.
      */
     private final CustomerRepository customerRepository;
-    public CustomerService(CustomerRepository customerRepository) {
+    private final CustomerDtoConverter customerDtoConverter;
+    public CustomerService(CustomerRepository customerRepository, CustomerDtoConverter customerDtoConverter) {
         this.customerRepository = customerRepository;
+        this.customerDtoConverter = customerDtoConverter;
     }
 
     public Customer findById(String id) {
         return customerRepository.findById(id).orElseThrow(()-> new CustomerNotFoundException(id +"'li kullanıcı bulunamdı!"));
+    }
+
+    public CustomerDto getCustomerDtoById(String id){
+        return customerDtoConverter.customerDtoFromCustomer(Optional.of(findById(id)));
     }
 }
